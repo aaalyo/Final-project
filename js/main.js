38a321d5ac7458a3e49aa4aef1ccc2cd60beabeb
aaalyo <67644184+aaalyo@users.noreply.github.com>
2021-04-18 17:55:33 +0300

    all

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 
 $('.card-title:first').load('https://aaalyo.github.io/Final-project/bob.html h6');
-
 $('.card-title:nth(1)').load('https://aaalyo.github.io/Final-project/cole-marmalade.html h6');
-
 $('.card-title:last').load('https://aaalyo.github.io/Final-project/hamilton.html h6');
 
 $('.card-text:first').load('https://aaalyo.github.io/Final-project/bob.html p');
+$('.card-text:nth(1)').load('https://aaalyo.github.io/Final-project/cole-marmalade.html p');
+$('.card-text:last').load('https://aaalyo.github.io/Final-project/hamilton.html p');
+
+$('.card-image:first').load('https://aaalyo.github.io/Final-project/hamilton.html .image-load');

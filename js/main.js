0c88d0938952edd71785d850be3d65aa5a9a84b1
aaalyo <67644184+aaalyo@users.noreply.github.com>
2021-04-22 00:15:54 +0300

    all

--- js/main.js
+++ js/main.js
@@ -158,9 +158,13 @@ $('.date-loaded-card-3').load('https://aaalyo.github.io/Final-project/hamilton.h
 
 
 function myMap() {
-    let mapProp = {
-        center: new google.maps.LatLng(56.959514, 24.239138),
-        zoom: 17,
+    const mapProp = {
+        center: new google.maps.LatLng(51.33765, 0.60302),
+        zoom: 15,
     };
     map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    const marker = new google.maps.Marker({
+        position: {lat:51.33765, lng:0.60302},
+        map: map,
+      });
 }
